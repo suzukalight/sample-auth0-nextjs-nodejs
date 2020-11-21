e09c9a077ca8679554cb3235935465e972b46d8a
suzukalight <mail@mkubara.com>
2020-11-21 14:27:16 +0900

    Add user repository using typeorm

---
+++ src/backend/src/repository/typeorm/user/queryService/User.ts
@@ -0,0 +1,33 @@
+import { Connection, Repository } from 'typeorm';
+import { denyIfNotSet } from '../../../../policy/decision/common';
+import {
+  GetUserByIdQuery,
+  GetUserByIdQueryResult,
+  UserQueryService,
+} from '../../../../usecase/query/user';
+import { User as OrmUser, OrmUserFactory } from '../entity/User';
+
+export class GqlUserQueryService implements UserQueryService {
+  private dbConnection: Connection;
+  private repository: Repository<OrmUser>;
+
+  constructor(dbConnection: Connection) {
+    this.dbConnection = dbConnection;
+    this.repository = this.dbConnection.getRepository(OrmUser);
+  }
+
+  public async getUserById(query: GetUserByIdQuery) {
+    denyIfNotSet(query, ['id']);
+    const { id } = query;
+
+    const result = await this.repository.findOne(id, {
+      relations: ['todos'], // eager loading で resolver の負荷を下げる
+    });
+    if (!result) return { user: null };
+
+    const res: GetUserByIdQueryResult = {
+      user: OrmUserFactory.toDto(result),
+    };
+    return res;
+  }
+}
