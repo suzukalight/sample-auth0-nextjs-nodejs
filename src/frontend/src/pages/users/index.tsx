6e8c1abb0eb805147c5aed139fb73ecfef1ec69e
suzukalight <mail@mkubara.com>
2020-11-23 18:30:47 +0900

    Add uset list page

---
+++ src/frontend/src/pages/users/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { UserList } from '../../components/pages/users';
+
+export const UserListPage: React.FC = () => {
+  return <UserList />;
+};
+
+export default UserListPage;
