a4260af6980f5badb151feafe2b5e74fe954af1a
suzukalight <mail@mkubara.com>
2020-11-23 22:03:47 +0900

    Add presenter

---
+++ src/backend/src/presenter/user/AllUsers.ts
@@ -0,0 +1,19 @@
+import { User } from 'schema';
+
+import {
+  AllUsersOutputData,
+  AllUsersPresenter,
+} from '../../usecase/query/user/interface/presenter';
+import { toGqlUser } from '../utils/converter/user';
+
+export class GqlAllUsersPresenter implements AllUsersPresenter {
+  private response: User[] | null = null;
+
+  public getResponse() {
+    return this.response;
+  }
+
+  public async output(response: AllUsersOutputData) {
+    this.response = response?.users?.map((user) => toGqlUser(user)!) || null;
+  }
+}
