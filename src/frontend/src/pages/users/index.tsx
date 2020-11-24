3cbd23322886fa12a1fc344c46c9b67414f04e7d
suzukalight <mail@mkubara.com>
2020-11-24 13:08:09 +0900

    Add login/logout process

--- src/frontend/src/pages/users/index.tsx
+++ src/frontend/src/pages/users/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
 import { UserList } from '../../components/pages/users';
+import { useFetchUser } from '../../hooks/useUser';
 import { useAllUsersQuery, User } from '../../_generated/graphql-client';
 
 export const UserListPage: React.FC = () => {
+  const { user, loading } = useFetchUser();
   const data = useAllUsersQuery();
+
+  if (loading) return <div>loading...</div>;
+  if (!user) return <div>user not found.</div>;
+
   if (!data) return null;
 
   const { users } = data?.data || {};
