49ca231cfe2038e950e23f42a134c67abb142f45
suzukalight <mail@mkubara.com>
2020-11-28 18:11:34 +0900

    Add styles

--- src/frontend-react/src/components/atoms/LoginButton/index.tsx
+++ src/frontend-react/src/components/atoms/LoginButton/index.tsx
@@ -4,7 +4,14 @@ import { useAuth0 } from '@auth0/auth0-react';
 const LoginButton: React.FC = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return (
+    <button
+      className="mr-5 bg-blue-700 text-white border border-blue-700 font-bold py-2 px-6 rounded-md"
+      onClick={() => loginWithRedirect()}
+    >
+      Log In
+    </button>
+  );
 };
 
 export default LoginButton;
--- src/frontend-react/src/components/atoms/LogoutButton/index.tsx
+++ src/frontend-react/src/components/atoms/LogoutButton/index.tsx
@@ -4,7 +4,14 @@ import { useAuth0 } from '@auth0/auth0-react';
 const LogoutButton: React.FC = () => {
   const { logout } = useAuth0();
 
-  return <button onClick={() => logout({ returnTo: window.location.origin })}>Log Out</button>;
+  return (
+    <button
+      className="mr-5 bg-gray-200 hover:bg-gray-300 border border-gray-400 text-black font-bold py-2 px-6 rounded-md"
+      onClick={() => logout({ returnTo: window.location.origin })}
+    >
+      Log Out
+    </button>
+  );
 };
 
 export default LogoutButton;
